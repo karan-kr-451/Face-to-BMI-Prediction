eee71ab32397398b091cf15bf0a351f28fe0cfe0
Karan Kumar <108926297+karan-kr-451@users.noreply.github.com>
2024-02-08 21:31:47 +0530

    Base Model Created

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from BMI import logger
 from BMI.pipeline.Stage_1 import DataIngestionTrainingPipeline
-
+from BMI.pipeline.Stage_2 import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 try:
@@ -11,3 +11,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = 'Base Model Build'
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/BMI/entity/config_entity.py
+++ src/BMI/entity/config_entity.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
+from typing import List
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
@@ -7,3 +8,19 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_augmentation: bool
+    params_image_size: List[int]
+    params_batch_size: int
+    params_include_top: bool
+    params_epochs: int
+    params_output_variables: List[dict]
+    params_weights: str
+    params_learning_rate: float
