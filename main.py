b6ad623a6d27c68c847245b74040762baf1ff879
Karan Kumar <108926297+karan-kr-451@users.noreply.github.com>
2024-02-08 19:15:43 +0530

    Data Ingestion Prepared

---
+++ main.py
@@ -0,0 +1,13 @@
+from BMI import logger
+from BMI.pipeline.Stage_1 import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = 'Data Ingestion Stage'
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
